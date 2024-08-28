e57f8c2f65858502d697daa3771a7454e7af2678
Mordran <100147006+Mordran@users.noreply.github.com>
2024-08-28 12:04:21 -0600

    Add files via upload

---
+++ stock.h
@@ -0,0 +1,21 @@
+#ifndef STOCK_H
+#define STOCK_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif /*__cplusplus */
+
+    typedef struct 
+    {
+        char code[8];
+        float price;
+
+    }Stock;
+    
+    
+Stock newStock(char code[], float price);
+#ifdef __cplusplus
+} /* extern "C" */
+#endif /*__cplusplus*/
+
+#endif /*HELP_H*/
---
+++ user.h
@@ -0,0 +1,21 @@
+#ifndef USER_H
+#define USER_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif /*__cplusplus */
+
+    typedef struct 
+    {
+        int index;
+        float money;
+
+    }User;
+    
+    
+User newUser(int index, float money);
+#ifdef __cplusplus
+} /* extern "C" */
+#endif /*__cplusplus*/
+
+#endif /*HELP_H*/
